06e2f1435f18ecc294e1bbecb4653ee5dc4683ba
Manpreet-kour <131549247+Manpreet-kour@users.noreply.github.com>
2024-10-13 15:25:24 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <>
+    <h2>
+      Budget
+    </h2>
+    <p>Good job,So far you're Keeping it under the budget</p>
+    </>
+  )
+}
